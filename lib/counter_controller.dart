0ab7302acac8f55bf6457fbdab6cd457ca203243
Yazid <yazidalrasyid821@gmail.com>
2026-02-10 10:13:30 +0700

    penambahan list untuk riwayat maksimal 5 perubahan

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,25 +1,48 @@
 class CounterController {
-  int _counter = 0; // Variabel private state
+  int _counter = 0;
   int _step = 1;
 
-  int get value => _counter; // Getter nilai counter
+  //penambahan variabel list untuk history atau riwayat
+  final List<String> _history = [];
+
+  int get value => _counter;
   int get step => _step;
+
+  //getter untuk mengambil riwayat atau history
+  List<String> get history => _history;
 
   void setStep(int val) {
     _step = val;
   }
 
+  // untuk pencatatan history
   void increment() {
     _counter += _step;
+    _addHistory("Ditambah $_step menjadi $_counter");
   }
 
   void decrement() {
     if (_counter >= _step) {
       _counter -= _step;
+      _addHistory("Dikurang $_step menjadi $_counter");
     } else {
       _counter = 0;
+      _addHistory("Dikurang mentok ke 0");
     }
   }
 
-  void reset() => _counter = 0;
+  //untuk hapus history saat melakukan reset
+  void reset() {
+    _counter = 0;
+    _history.clear();
+  }
+
+  // untuk memeriksa data lama yang lebih dari 5 (perubahan ke terbaru)
+  void _addHistory(String message) {
+    _history.insert(0, message);
+
+    if (_history.length > 5) {
+      _history.removeLast();
+    }
+  }
 }
--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -14,40 +14,64 @@ class _CounterViewState extends State<CounterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("LogBook SRP bagian Task 1"),
+        title: const Text("LogBook: SRP"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // menampilkan step
-              Text("Step: ${_controller.step}", style: const TextStyle(fontSize: 20)),
+      // bagian dari Slider sama site button
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                // menampilkan step
+                Text("Step: ${_controller.step}", style: const TextStyle(fontSize: 20)),
+                // slider untuk mengubah nilai Step
+                Slider(
+                  value: _controller.step.toDouble(),
+                  min: 1,
+                  max: 10,
+                  divisions: 9,
+                  label: _controller.step.toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      _controller.setStep(value.toInt());
+                    });
+                  },
+                ),
+                const Divider(),
+                const Text("Total Hitungan:"),
+                Text(
+                  '${_controller.value}',
+                  style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
 
-              //slider untuk mengubah nilai Step
-              Slider(
-                value: _controller.step.toDouble(),
-                min: 1,
-                max: 10,
-                divisions: 9,
-                label: _controller.step.toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    _controller.setStep(value.toInt());
-                  });
+            const SizedBox(height: 20),
+
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Riwayat Perubahan :", style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+
+            Expanded(
+              // pembuatan list riwayat perubahan sebanyak 5 perubahan
+              child: ListView.builder(
+                itemCount: _controller.history.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 2,
+                    margin: const EdgeInsets.symmetric(vertical: 5),
+                    child: ListTile(
+                      leading: const Icon(Icons.history, color: Colors.blue),
+                      title: Text(_controller.history[index]),
+                      subtitle: Text("Data ke-${index + 1}"),
+                    ),
+                  );
                 },
               ),
-
-              const Divider(), // Garis pemisah estetis
-
-              const Text("Total Hitungan:"),
-              Text(
-                '${_controller.value}',
-                style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       // menampilkan button (reset, kurang, tambah)
@@ -60,17 +84,15 @@ class _CounterViewState extends State<CounterView> {
             backgroundColor: Colors.red,
             child: const Icon(Icons.refresh),
           ),
-          const SizedBox(width: 10),
-
           // tombol decrement (-)
+          const SizedBox(width: 10),
           FloatingActionButton(
             onPressed: () => setState(() => _controller.decrement()),
             backgroundColor: Colors.orange,
             child: const Icon(Icons.remove),
           ),
-          const SizedBox(width: 10),
-
           // tombol increment (+)
+          const SizedBox(width: 10),
           FloatingActionButton(
             onPressed: () => setState(() => _controller.increment()),
             child: const Icon(Icons.add),
