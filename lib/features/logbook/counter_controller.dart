bca364ac2fc1a4b186de257aac86881ea64463a3
Yazid <yazidalrasyid821@gmail.com>
2026-02-19 10:41:03 +0700

    Tambahkan fungsi untuk memuat dan menyimpan riwayat tindakan pengguna serta perbarui format catatan riwayat.

--- lib/features/logbook/counter_controller.dart
+++ lib/features/logbook/counter_controller.dart
@@ -1,45 +1,56 @@
 import 'package:shared_preferences/shared_preferences.dart';
+
 class CounterController {
   int _counter = 0;
   int _step = 1;
 
-  //penambahan variabel list untuk history atau riwayat
-  final List<String> _history = [];
-
+  //variabel untuk menyimpan riwayat atau history
+  List<String> _history = [];
   String _activeUser = "";
-
   int get value => _counter;
   int get step => _step;
 
-  //getter untuk mengambil riwayat atau history
+  // getter untuk mengambil riwayat atau history
   List<String> get history => _history;
 
   Future<void> initData(String username) async {
     _activeUser = username;
     _counter = await loadLastValue(username);
+    _history = await loadHistory(username);
   }
 
-  // Fungsi untuk memuat data dari SharedPreferences
   Future<int> loadLastValue(String username) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt('${username}_counter') ?? 0;
   }
 
-  // Fungsi untuk menyimpan data ke SharedPreferences
   Future<void> saveLastValue(int value, String username) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('${username}_counter', value);
   }
 
-  // Fungsi untuk menambahkan catatan ke riwayat
+  //memuat fungsi baru untuk memuat riwayat history dari memori HP
+  Future<List<String>> loadHistory(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('${username}_history') ?? [];
+  }
+
+  //menyimpan fungsi untuk menyimpan riwayat history ke memori HP
+  Future<void> saveHistory(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('${username}_history', _history);
+  }
+
+  //fungsi untuk menambahkan riwayat atau history
   void _addHistory(String action, int amount) {
-    _history.insert(0, "$action $amount");
+    final time = DateTime.now().toString().substring(11, 16);
+    _history.insert(0, "[$time] $action $amount");
     if (_history.length > 5) {
       _history.removeLast();
     }
+    saveHistory(_activeUser);
   }
 
-  // untuk pencatatan history
   void increment() {
     _counter += _step;
     _addHistory("Ditambah", _step);
@@ -55,8 +66,10 @@ class CounterController {
   void reset() {
     _counter = 0;
     _history.clear();
-    _history.insert(0, "Data di-reset ke 0");
+    final time = DateTime.now().toString().substring(11, 16);
+    _history.insert(0, "[$time] Data di-reset ke 0");
     saveLastValue(_counter, _activeUser);
+    saveHistory(_activeUser);
   }
 
   void setStep(int newStep) {
