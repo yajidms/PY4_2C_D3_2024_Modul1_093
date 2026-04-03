790ca3a9b3ce1c2932270f4dcb1b921b2b87e4ab
Yazid <yazidalrasyid821@gmail.com>
2026-04-03 08:25:58 +0700

    Perbarui nama aplikasi dan namespace dari logbook_app_001 menjadi logbook_app_093

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.logbook_app_001"
+    namespace = "com.example.logbook_app_093"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.logbook_app_001"
+        applicationId = "com.example.logbook_app_093"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
